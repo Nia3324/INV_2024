3e565709c972b552d4b9906ce4829e4b4d516149
Nia <antonia.c.brito2@gmail.com>
2024-07-24 18:42:30 +0100

    Update explainability.py

--- explainability.py
+++ explainability.py
@@ -64,7 +64,7 @@ def algorithm_class_score(model, verbose=False):
     
     # If no match is found, return None
     if verbose:
-        print(f"No matching score found for '{model_name}'")
+        print(f"No matching score found for '{model_name}'!")
     return None
 
 
@@ -118,10 +118,11 @@ def correlated_features_score(dataset, target_column=None, verbose=False):
     
     # Calculate the proportion of features removed due to high correlation
     pct_drop = len(to_drop) / len(X_test.columns)
-    return (1 - pct_drop)  # Return the score as the proportion of non-highly correlated features
+    if verbose: print("Proportion of non-highly correlated features.")
+    return (1 - pct_drop)  
 
 
-def model_size(model, test_dataset=None):
+def model_size(model, test_dataset=None, verbose=False):
     """
     Calculate the size of a machine learning model based on its type.
     
@@ -140,10 +141,12 @@ def model_size(model, test_dataset=None):
     
     # If the model is a TensorFlow Keras Model, return the count of parameters
     if isinstance(model, tf.keras.Model):
+        if verbose: print("Returned number of parameters")
         return model.count_params()
     
     # If the model is a TensorFlow Module or PyTorch Module, return the sum of parameters
     elif isinstance(model, tf.Module) or isinstance(model, nn.Module):
+        if verbose: print("Returned number of parameters")
         return sum(p.numel() for p in model.parameters())
     
     # If the model has 'estimators_', typically an ensemble model like RandomForest
@@ -152,34 +155,41 @@ def model_size(model, test_dataset=None):
         for i, est in enumerate(model.estimators_):
             # If the estimator has a tree structure, add the number of nodes
             if hasattr(est, 'tree_'):
+                if verbose: print("Returned number of total nodes")
                 count += est.tree_.node_count
             # If the estimator has support vectors, add their count
             elif hasattr(est, 'n_support_'):
+                if verbose: print("Returned number of total support vectors")
                 count += sum(est.n_support_)
         return count
     
     # If the model is a Support Vector Classifier
     elif hasattr(model, 'SVC'):
+        if verbose: print("Returned number of support vectors")
         return sum(model.n_support_)
 
     # If the model has a tree structure, return the number of nodes
     elif hasattr(model, 'tree_'):
+        if verbose: print("Returned number of nodes")
         return model.tree_.node_count
     
     # Return the number of features seen during fit if applicable
     elif hasattr(model, 'n_features_in_'):
+        if verbose: print("Returned number of features seen during fit.")
         return 'n_features_in_'
     
     # If a test dataset is provided, return the number of features in the dataset
     elif test_dataset is not None:
+        if verbose: print("Returned number of features in test_dataset.")
         return test_dataset.shape[1]
     
     # If none of the above conditions are met, return None
     else:
+        if verbose: print("Unable to determine model size!")
         return None
 
 
-def feature_importance_score(model):
+def feature_importance_score(model, verbose=False):
     """
     Calculate the feature importance score for a given classifier.
     
@@ -218,7 +228,8 @@ def feature_importance_score(model):
         importance = model.feature_importances_
     
     else:
-        return None  # Return None if the classifier type is not supported
+        if verbose: print("Classifier type is not supported!")
+        return None  
 
     # Sort the importance values in descending order
     indices = np.argsort(importance)[::-1]
@@ -227,6 +238,7 @@ def feature_importance_score(model):
     # Calculate the percentage of features that concentrate distri_threshold percent of the total importance
     pct_dist = sum(np.cumsum(importance) < distri_threshold) / len(importance)
     
+    if verbose: print(f"Percentage of features that concentrate distri_threshold ({distri_threshold}) percent of the total importance.")
     return pct_dist
 
 
@@ -246,7 +258,7 @@ def predict(model):
     return model.predict_proba if hasattr(model, 'predict_proba') else model.predict
 
 
-def cv_shap_score(model, test_dataset):
+def cv_shap_score(model, test_dataset, verbose=False):
     """
     Calculate the coefficient of variation (CV) of SHAP values for a classifier.
     
@@ -277,8 +289,10 @@ def cv_shap_score(model, test_dataset):
         sums = np.array([abs(shap_values[i]).sum() for i in range(len(shap_values))])
         # Calculate the coefficient of variation (CV)
         cv = np.std(sums) / np.mean(sums)
+        if verbose: print("Coefficient of variation of absolute SHAP values.")
         return cv
     
     else:
-        return None  # Return None if SHAP values cannot be calculated
+        if verbose: print("SHAP values cannot be calculated!")
+        return None 
 
